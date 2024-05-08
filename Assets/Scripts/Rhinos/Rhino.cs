28415c997e562627179ed265c70bb894bd1a11ed
JordanZunde <jordanzunde@gmail.com>
2024-05-08 17:25:48 +0200

    Rhino state machine, Waypoint manager, Navmesh, FInished prefabs

--- Assets/Scripts/Rhinos/Rhino.cs
+++ Assets/Scripts/Rhinos/Rhino.cs
@@ -1,22 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Rhino : MonoBehaviour
 {
     public float currentHappiness, currentCleanliness, currentHealth, currentSleep, currentHunger, currentActivity;
-
+    public Vector3[] waypoints = new Vector3[12];
+    public Vector3 currentTarget;
     public RhinoScriptable rhinoScript;
+    public RhinoAction currentAction, previousAction;
+    public WaypointManager _WaypointManager;
+    
+    public enum RhinoAction
+    {
+        Idle,
+        Eat,
+        Clean,
+        Play,
+        Sleep
+    }
+
+    public void ChangeAction(RhinoAction desiredAction)
+    {
+        if (currentAction == desiredAction)
+        {
+            return; 
+        }
+
+        if (currentAction != desiredAction)
+        {
+            previousAction = currentAction;
+            currentAction = desiredAction;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         rhinoScript = gameObject.GetComponent<RhinoScriptable>();
+        _WaypointManager = GameObject.FindGameObjectWithTag("WaypointManager").GetComponent<WaypointManager>();
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = _WaypointManager.waypoints[i].transform.position;
+        }
+        ChangeAction(RhinoAction.Idle);
+    }
+
+    public void Idle()
+    {
+        int i = Random.Range(0, 12);
+        currentTarget = waypoints[i];
+    }
+
+    public void Eat()
+    {
         
     }
 
+    public void Clean()
+    {
+        
+    }
+
+    public void Play()
+    {
+        
+    }
+
+    public void Sleep()
+    {
+        
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentAction)
+        {
+            case RhinoAction.Idle:
+                Idle();
+                break;
+            case RhinoAction.Eat:
+                Eat();
+                break;
+            case RhinoAction.Clean:
+                Clean();
+                break;
+            case RhinoAction.Play:
+                Play();
+                break;
+            case RhinoAction.Sleep:
+                Sleep();
+                break;
+            default:
+                break;
+        }  
     }
 }
