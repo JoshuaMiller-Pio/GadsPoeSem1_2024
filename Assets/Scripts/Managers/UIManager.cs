ca4b5bff1bdcf05e796efff5f4a719a77e15f854
JordanZunde <jordanzunde@gmail.com>
2024-05-19 18:10:34 +0200

    MainMenu

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -9,10 +9,11 @@ public class UIManager : MonoBehaviour
     public TMP_Text rhinoName, currentHealth, currentHappiness, currentSleep, currentCleanliness, currentExecercise;
 
     public Image rhinoPhoto;
+    public Canvas rhnoActions;
 
     public void UpdateRhinoInfo(Rhino selectedRhino)
     {
-        
+        rhnoActions.gameObject.SetActive(true);
     }
     // Start is called before the first frame update
     void Start()
