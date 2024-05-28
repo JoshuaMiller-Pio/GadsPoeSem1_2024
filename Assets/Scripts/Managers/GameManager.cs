294ff518e9f30764b119e3bb7b1a47343aff759c
JordanZunde <jordanzunde@gmail.com>
2024-05-28 19:45:54 +0200

    RhinoSpawning

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : Singleton<GameManager>
 {
+    public GameObject maleRhinoPrefab, femaleRhinoPrefab;
     public Rhino chosenRhino { get; set; }
-   
+    public List<GameObject> rhinos = new List<GameObject>(11);
     public int currentGold, foodCost = 10, medsCost = 20, cleanCost = 20;
+    private PrefabManager _prefabManager;
+    private WaypointManager _waypointManager;
+
+    public int  maxSpawnedRhinos = 2;
     // Start is called before the first frame update
     void Start()
     {
+        
         currentGold = 30;
-      
+       // maleRhinoPrefab = GameObject.FindGameObjectWithTag("MaleRhino");
+        //femaleRhinoPrefab = GameObject.FindGameObjectWithTag("FemaleRhino");
+        if (SceneManager.GetSceneByBuildIndex(1) == SceneManager.GetActiveScene())
+        {
+            if (rhinos.Count == 0)
+            {
+                _prefabManager = GameObject.FindGameObjectWithTag("PrefabManager").GetComponent<PrefabManager>();
+                maleRhinoPrefab = _prefabManager.malePrefab;
+                femaleRhinoPrefab = _prefabManager.femalePrefab;
+                _waypointManager = GameObject.FindGameObjectWithTag("WaypointManager").GetComponent<WaypointManager>();
+                CreateRhinos(); 
+            }
+            
+        }
+       
     }
 
     public void PlayRhinoRun()
@@ -23,6 +45,35 @@ public class GameManager : Singleton<GameManager>
         SceneManager.LoadScene(3);
     }
 
+    public void CreateRhinos()
+    {
+        for (int t = 0; t < 10; t++)
+        {
+            int i = Random.Range(1, 3);
+            if (i == 0)
+            {
+                GameObject newRhino = maleRhinoPrefab;
+                rhinos.Add(newRhino);
+                int p = Random.Range(3, 10);
+                Instantiate(newRhino, _waypointManager.waypoints[p].transform.position, quaternion.identity);
+            }
+            else
+            {
+                GameObject newRhino = maleRhinoPrefab;
+                rhinos.Add(newRhino);
+                int p = Random.Range(3, 10);
+                Instantiate(newRhino, _waypointManager.waypoints[p].transform.position, quaternion.identity);
+            }
+        }
+
+        for (int i = 3; i < 9; i++)
+        {
+            
+                rhinos[i].gameObject.SetActive(false);
+            
+            
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
